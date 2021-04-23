849944d4e4ce8fd2c471b9d62767dba7aaab20d9
Delowar Hossain <delowardev@gmail.com>
2021-04-24 02:11:24 +0600

    setting up

--- src/Select.svelte
+++ src/Select.svelte
@@ -1,7 +1,9 @@
 <script lang="ts">
-	import { get } from "svelte/store";
+	import {get} from "svelte/store";
 	import state from "./store";
 	import type {State} from "./interface";
+	import ChevronDown from "./components/icons/ChevronDown.svelte";
+	import Times from "./components/icons/Times.svelte";
 
 	/**
 	 * Props
@@ -12,10 +14,10 @@
 	/**
 	 * Set initial value
 	 */
-	state.update((prevState) : State => {
+	state.update((prevState): State => {
 		const _options: HTMLCollectionOf<HTMLOptionElement> = select.selectedOptions;
-		const options = Array.from(select.options).map(({ value, text }) => ({value, text}));
-		const values = Array.from(_options).map(({ value, text }) => ({value, text}));
+		const options = Array.from(select.options).map(({value, text}) => ({value, text}));
+		const values = Array.from(_options).map(({value, text}) => ({value, text}));
 		const value = values[0] || options[0];
 		const multiple = select.multiple;
 		return {
@@ -33,7 +35,7 @@
 
 	const getState = get(state);
 	const getValue = getState.value?.value || "";
-	const className = `v2select ` + (getState.multiple ? `v2select__multiple`: `v2select__single`);
+	const className = `v2select ` + (getState.multiple ? `v2select__multiple` : `v2select__single`);
 	let search = "";
 
 	/**
@@ -50,11 +52,16 @@
 	<div class={className}>
 		<div class="v2select__controls">
 			<div class="v2select__values">
-				<input type="text" bind:value={search}>
+				<input class="v2select__search" type="text" bind:value={search}>
 				<div class="v2select__value">{getValue}</div>
 			</div>
 			<div class="v2select__buttons">
-				x
+				<button class="v2select__times v2select__icon v2select__btn">
+					<Times />
+				</button>
+				<button class="v2select__arrow v2select__icon v2select__btn">
+					<ChevronDown />
+				</button>
 			</div>
 		</div>
 		<div class="v2select__dropdown">
