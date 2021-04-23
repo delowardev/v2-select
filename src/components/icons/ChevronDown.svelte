8ffeb4c46c739caaa93c5d0d598672c49980146a
Delowar Hossain <delowardev@gmail.com>
2021-04-24 02:25:11 +0600

    icons added

--- src/components/icons/ChevronDown.svelte
+++ src/components/icons/ChevronDown.svelte
@@ -1,3 +1,10 @@
-<svg xmlns="http://www.w3.org/2000/svg" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-chevron-down" viewBox="0 0 24 24">
+<svg xmlns="http://www.w3.org/2000/svg" fill="none" stroke="currentColor" stroke-width="3" stroke-linecap="round" stroke-linejoin="round" class="feather feather-chevron-down" viewBox="0 0 24 24">
     <path d="M6 9l6 6 6-6"/>
 </svg>
+
+<style>
+    svg {
+        height: 1em;
+        width: 1em;
+    }
+</style>
